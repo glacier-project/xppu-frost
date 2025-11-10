6ca86bd31b84d16845da84db5f337bf767668635
Sebastiano <gaia.franseba@gmail.com>
2025-11-10 14:35:37 +0100

    feat(crane): Add RBase, MDriver and Motor reactors

---
+++ src/python-lib/crane_utils.py
@@ -0,0 +1,18 @@
+from enum import Enum
+
+class MotorCMD(str, Enum):
+    IDLE = 'IDLE'
+    CW = 'CW' # clockwise
+    CCW = 'CCW' # counterclockwise
+
+class CraneCmd(str, Enum):
+    ROTATE_S = "ROTATE_S",
+    ROTATE_LSC = "ROTATE_LSC",
+    ROTATE_ST = "ROTATE_ST",
+    PICKUP_WP = "PICKUP_WP",
+    PUTDOWN_WP = "PUTDOWN_WP"
+
+class CranePos(str, Enum):
+    POS_S = "POS_S"
+    POS_LSC = "POS_LSC"
+    POS_ST = "POS_ST"
